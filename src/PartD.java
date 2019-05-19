c49d20b390524f209cf6c8b691aff2f5fa080691
DeltaDivingNZ <deltadivingnz@gmail.com>
2019-05-19 17:19:16 +1200

    Favourite Day

--- src/PartD.java
+++ src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("Hello, my name is Chris");
 		System.out.println("My favourite colour is green");
 		System.out.println("My favourite mode of transportation is boat");
+		System.out.println("The best day of the week is Friday");
 
 	}
 
