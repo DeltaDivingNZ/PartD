55997237e522317b1270a85f91d2edf38d29089d
DeltaDivingNZ <deltadivingnz@gmail.com>
2019-05-19 17:18:36 +1200

    Favourite transportation

--- src/PartD.java
+++ src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Chris");
 		System.out.println("My favourite colour is green");
+		System.out.println("My favourite mode of transportation is boat");
 
 	}
 
